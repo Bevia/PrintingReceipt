31ad0858a9afca93340f90008c6a59e801de5455
vbevia <vbevia@gmail.com>
2025-05-14 15:17:03 +0200

    extract individual values

--- src/main/java/org/example/JSONMapper.java
+++ src/main/java/org/example/JSONMapper.java
@@ -7,8 +7,8 @@ import java.util.HashMap;
 public class JSONMapper {
     public static void main(String[] args) {
         // Your JSON string
-        //JSONObject jsonObject = getJsonObjectWithRelatedTransactions();
-        JSONObject jsonObject = getJsonObject();
+        JSONObject jsonObject = getJsonObjectWithRelatedTransactions();
+        //JSONObject jsonObject = getJsonObject();
 
         System.out.println(jsonObject.toString(4));
 
@@ -26,6 +26,9 @@ public class JSONMapper {
                 : "Not available");
         mappedData.put("cardholder_verification_method", data.has("payment") && data.getJSONObject("payment").has("cardholder_verification_method")
                 ? data.getJSONObject("payment").getString("cardholder_verification_method")
+                : "Not available");
+        mappedData.put("card_acceptor_location", data.has("payment") && data.getJSONObject("payment").has("card_acceptor_location")
+                ? data.getJSONObject("payment").getString("card_acceptor_location")
                 : "Not available");
         mappedData.put("card_entry_mode", data.has("payment") && data.getJSONObject("payment").has("card_entry_mode")
                 ? data.getJSONObject("payment").getString("card_entry_mode")
@@ -59,72 +62,81 @@ public class JSONMapper {
         // Print mapped data
         System.out.println("Validated JSON Data:");
         mappedData.forEach((key, value) -> System.out.println(key + " : " + value));
+
+        // Get specific values from the mappedData HashMap
+        String card_acceptor_location = mappedData.getOrDefault("card_acceptor_location", "Not available");
+        String merchantAddress = mappedData.getOrDefault("merchant_address", "Not available");
+        String cardholderVerificationMethod = mappedData.getOrDefault("cardholder_verification_method", "Not available");
+
+
+        System.out.println("\nIndividually extracted values:");
+        System.out.println("Card Acceptor Location: " + card_acceptor_location);
+        System.out.println("Merchant Address: " + merchantAddress);
+        System.out.println("Cardholder Verification Method: " + cardholderVerificationMethod);
     }
 
     private static JSONObject getJsonObjectWithRelatedTransactions() {
         String jsonString = "{"
                 + "\"data\": {"
                 + "\"merchant\": {"
-                + "\"address\": \"Rue de la Gares test1 fdsas\","
+                + "\"address\": \"Vijzelstraat Amsterdam\","
                 + "\"name\": \"MultiSafepay B.V.\""
                 + "},"
                 + "\"order\": {"
-                + "\"amount\": 150,"
-                + "\"amountrefunded\": 75,"
-                + "\"completed\": \"2024-09-10T10:03:50\","
+                + "\"amount\": 15,"
+                + "\"amountrefunded\": 15,"
+                + "\"completed\": \"2025-05-14T14:57:17\","
                 + "\"currency\": \"EUR\","
                 + "\"items\": null,"
-                + "\"tip\": {"
-                + "\"amount\": 50"
-                + "},"
-                + "\"transaction_id\": \"1725955429106316\""
+                + "\"tip\": null,"
+                + "\"transaction_id\": \"1747227436104103\""
                 + "},"
                 + "\"payment\": {"
-                + "\"application_id\": \"a0000000041010\","
-                + "\"authorization_code\": \"006459\","
-                + "\"card_acceptor_location\": \"fdsas\","
+                + "\"application_id\": \"a0000000031010\","
+                + "\"authorization_code\": \"UU1YSN\","
+                + "\"card_acceptor_location\": \"Amsterdam\","
                 + "\"card_entry_mode\": \"ICC_CONTACTLESS\","
-                + "\"card_expiry_date\": \"2709\","
-                + "\"cardholder_verification_method\": \"PLAINTEXT_PIN_OFFLINE\","
-                + "\"issuer_bin\": \"516788\","
-                + "\"issuer_country_code\": \"ES\","
-                + "\"last4\": \"5477\","
-                + "\"payment_method\": \"MASTERCARD\","
+                + "\"card_expiry_date\": \"3112\","
+                + "\"cardholder_verification_method\": \"FAILED\","
+                + "\"issuer_bin\": \"416598\","
+                + "\"issuer_country_code\": \"LT\","
+                + "\"last4\": \"6777\","
+                + "\"payment_method\": \"VISA\","
                 + "\"response_code\": \"00\","
-                + "\"terminal_id\": \"0000037E\""
+                + "\"terminal_id\": \"000006F3\""
                 + "},"
-                + "\"printed_on\": \"2024-12-03T12:18:50\","
+                + "\"printed_on\": \"2025-05-14T14:59:02\","
                 + "\"related_transactions\": ["
                 + "{"
-                + "\"amount\": 25,"
-                + "\"created\": \"2024-09-10T10:05:06\","
+                + "\"amount\": 10,"
+                + "\"created\": \"2025-05-14T14:57:41\","
                 + "\"currency\": \"EUR\","
-                + "\"description\": \"Refund order 310355912 for Alipay+POS\","
+                + "\"description\": \"Refund order 86231067 for Alipay+POS\","
                 + "\"items\": null,"
-                + "\"modified\": \"2024-09-10T10:05:06\","
-                + "\"order_id\": \"310355912\","
+                + "\"modified\": \"2025-05-14T14:57:41\","
+                + "\"order_id\": \"86231067\","
                 + "\"payment_details\": {"
-                + "\"acquirer_reference_number\": \"02692424254000001108772\""
+                + "\"acquirer_reference_number\": \"74347495134000462532670\""
                 + "},"
-                + "\"reference_transaction_id\": 1725955429106316,"
+                + "\"reference_transaction_id\": 1747227436104103,"
                 + "\"status\": \"completed\","
-                + "\"transaction_id\": 1725955506106330,"
+                + "\"transaction_id\": 1747227461107399,"
                 + "\"type\": \"refund\""
                 + "},"
                 + "{"
-                + "\"amount\": 50,"
-                + "\"created\": \"2024-09-10T10:05:29\","
+                + "\"amount\": 5,"
+                + "\"created\": \"2025-05-14T14:57:53\","
                 + "\"currency\": \"EUR\","
-                + "\"description\": \"Refund order 310355912 for Alipay+POS\","
+                + "\"description\": \"Refund order 86231067 for Alipay+POS\","
                 + "\"items\": null,"
-                + "\"modified\": \"2024-09-10T10:05:29\","
-                + "\"order_id\": \"310355912\","
+                + "\"modified\": \"2025-05-14T14:57:53\","
+                + "\"order_id\": \"86231067\","
                 + "\"payment_details\": {"
-                + "\"acquirer_reference_number\": \"02692424254000001108780\""
+                + "\"acquirer_reference_number\": \"74347495134000462532886\""
                 + "},"
-                + "\"reference_transaction_id\": 1725955429106316,"
+                + "\"reference_transaction_id\": 1747227436104103,"
                 + "\"status\": \"completed\","
-                + "\"transaction_id\": 1725955529106353,"
+                + "\"transaction_id\": 1747227473109273,"
                 + "\"type\": \"refund\""
                 + "}"
                 + "]"
@@ -137,36 +149,37 @@ public class JSONMapper {
     }
 
     private static JSONObject getJsonObject() {
+
         String jsonString = "{"
                 + "\"data\": {"
                 + "\"merchant\": {"
-                + "\"address\": \"Rue de la Gares test1 fdsas\","
+                + "\"address\": \"Vijzelstraat Amsterdam\","
                 + "\"name\": \"MultiSafepay B.V.\""
                 + "},"
                 + "\"order\": {"
-                + "\"amount\": 100,"
+                + "\"amount\": 1,"
                 + "\"amountrefunded\": 0,"
-                + "\"completed\": \"2024-12-19T14:16:13\","
+                + "\"completed\": \"2025-05-13T12:10:56\","
                 + "\"currency\": \"EUR\","
                 + "\"items\": null,"
                 + "\"tip\": null,"
-                + "\"transaction_id\": \"1734614172108670\""
+                + "\"transaction_id\": \"1747131054104206\""
                 + "},"
                 + "\"payment\": {"
-                + "\"application_id\": \"a0000000043060\","
-                + "\"authorization_code\": \"006459\","
-                + "\"card_acceptor_location\": \"fdsas\","
+                + "\"application_id\": \"a0000000041010\","
+                + "\"authorization_code\": \"426045\","
+                + "\"card_acceptor_location\": \"Amsterdam\","
                 + "\"card_entry_mode\": \"ICC_CONTACTLESS\","
-                + "\"card_expiry_date\": \"2512\","
-                + "\"cardholder_verification_method\": \"ENCIPHERED_PIN_ONLINE\","
-                + "\"issuer_bin\": \"679999\","
-                + "\"issuer_country_code\": null,"
-                + "\"last4\": \"2010\","
-                + "\"payment_method\": \"MAESTRO\","
+                + "\"card_expiry_date\": \"2908\","
+                + "\"cardholder_verification_method\": \"PLAINTEXT_PIN_OFFLINE\","
+                + "\"issuer_bin\": \"545366\","
+                + "\"issuer_country_code\": \"ES\","
+                + "\"last4\": \"2473\","
+                + "\"payment_method\": \"MASTERCARD\","
                 + "\"response_code\": \"00\","
-                + "\"terminal_id\": \"00000DKJ\""
+                + "\"terminal_id\": \"000006DT\""
                 + "},"
-                + "\"printed_on\": \"2025-05-12T09:38:26\","
+                + "\"printed_on\": \"2025-05-14T14:28:40\","
                 + "\"related_transactions\": null"
                 + "},"
                 + "\"success\": true"
